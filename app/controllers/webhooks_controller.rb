4f0473ff38e2c5648488fe8abd0d15701ce33df3
Eleanor Kiefel Haggerty <eleanorakh@gmail.com>
2017-03-25 19:44:21 +1100

    Set up webhook

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -2,6 +2,11 @@ class WebhooksController < ApplicationController
   protect_from_forgery if: -> { false }
 
   def rubygems
+    name = params.permit(:name)[:name]
+    recipients = Recipient.where(gem: name)
+    recipients.each do |recipient|
+      recipient.email
+    end
     head :ok
   end
 end
